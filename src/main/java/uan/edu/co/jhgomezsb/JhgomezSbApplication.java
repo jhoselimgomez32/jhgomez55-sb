762fc39867614d1ab9ed960a9c641406ee341c7f
jhoselimgomez32 <jhgomez55@uan.edu.co>
2022-04-01 22:37:11 -0500

    first commit

---
+++ src/main/java/uan/edu/co/jhgomezsb/JhgomezSbApplication.java
@@ -0,0 +1,13 @@
+package uan.edu.co.jhgomezsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhgomezSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhgomezSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uan/edu/co/jhgomezsb/JhgomezSbApplicationTests.java
@@ -0,0 +1,13 @@
+package uan.edu.co.jhgomezsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhgomezSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
