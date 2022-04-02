addaf0bab478fc1967eacb038386ae4b89a9b56c
jhoselimgomez32 <jhgomez55@uan.edu.co>
2022-04-01 23:21:52 -0500

    import

--- src/main/java/uan/edu/co/jhgomezsb/JhgomezSbApplication.java
+++ src/main/java/uan/edu/co/jhgomezsb/JhgomezSbApplication.java
@@ -2,7 +2,7 @@ package uan.edu.co.jhgomezsb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
